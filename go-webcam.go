dcd0b721ea761391975fb097b9441e299fbb8b4b
Ilnur Sayfutdinov <ilnursoft@gmail.com>
2016-01-06 21:57:25 +0300

    add main file project

--- go-webcam.go
+++ go-webcam.go
@@ -3,7 +3,35 @@ package main
 
 import (
 	"fmt"
+//		"io"
+		"net/http"
+	"io/ioutil"
+		"golang.org/x/net/html/charset"
 )
+
+
+//получение страницы из урла url
+func gethtmlpage(url string) []byte {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("HTTP error:", err)
+		panic("HTTP error")
+	}
+	defer resp.Body.Close()
+	// вот здесь и начинается самое интересное
+	utf8, err := charset.NewReader(resp.Body, resp.Header.Get("Content-Type"))
+	if err != nil {
+		fmt.Println("Encoding error:", err)
+		panic("Encoding error")
+	}
+	body, err := ioutil.ReadAll(utf8)
+	if err != nil {
+		fmt.Println("IO error:", err)
+		panic("IO error")
+	}
+	return body
+}
+
 
 func main() {
 	fmt.Println("Hello World!")
